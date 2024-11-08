3e0988c24c7f59d52f33abb50211c3caea9543f1
EvoShishir <atef.arman@yahoo.com>
2024-11-08 21:28:27 +0600

    added custom animated cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import AnimatedCursor from "react-animated-cursor";
 import "./App.css";
 import HomePage from "./Pages/HomePage/HomePage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <>
+      <AnimatedCursor
+        innerSize={5}
+        outerSize={25}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: "var(--cursor-color)",
+        }}
+        outerStyle={{
+          border: "3px solid var(--cursor-color)",
+        }}
+      />
+      <HomePage />
+    </>
+  );
 }
 
 export default App;
